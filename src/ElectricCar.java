c3cd491bd63dc12aefd8da32439e7b452702fbb8
tv0823 <tv0823@bs.amalnet.k12.il>
2024-11-02 17:10:06 +0200

    changed extend of ElectricCar

--- src/ElectricCar.java
+++ src/ElectricCar.java
@@ -1,8 +1,20 @@
-public class ElectricCar extends Vehicle
+public class ElectricCar extends Car
 {
     @Override
     public void accelerate()
     {
-        System.out.println("Im accelerating by pressing the flying option");
+        System.out.println("Im accelerating by pressing the magnet option");
+    }
+
+    @Override
+    public void start()
+    {
+        System.out.println("Put key card on the plat form");
+    }
+
+    @Override
+    public void stop()
+    {
+        System.out.println("Put out the key card");
     }
 }
